bd55cdeaea19680cf11da7028aec03f467f49518
Kayan Freitas <kayan6f@gmail.com>
2020-03-29 23:08:43 -0300

    Aplicação mobile

---
+++ mobile/src/routes.js
@@ -0,0 +1,21 @@
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+
+const AppStack = createStackNavigator();
+
+import Incidents from './pages/Incidents';
+import Details from './pages/Details';
+
+export default function Routes(){
+    return (
+        <NavigationContainer>
+
+            <AppStack.Navigator screenOptions={{ headerShown: false}}>
+            <AppStack.Screen name="Incidents" component={Incidents} />
+            <AppStack.Screen name="Details" component={Details} />
+            </AppStack.Navigator>
+
+        </NavigationContainer>
+    );
+}
